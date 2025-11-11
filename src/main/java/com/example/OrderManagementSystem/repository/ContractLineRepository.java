97f8ede7be4906c21caf543a7142057cfa4c53ee
davidandrus1 <david.andrus@stud.ubbcluj.ro>
2025-11-11 09:05:54 +0200

    Sters SellableItemRepository

--- src/main/java/com/example/OrderManagementSystem/repository/ContractLineRepository.java
+++ src/main/java/com/example/OrderManagementSystem/repository/ContractLineRepository.java
@@ -8,18 +8,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 
 public class ContractLineRepository extends BaseRepository<ContractLine>{
-    private int nextId = 1;
 
     @Override
     protected String getEntityId(ContractLine entity) {
         return entity.id;
     }
 
-    @Override
-    public void save(ContractLine entity) {
-        if (entity.id == null || entity.id.isEmpty()) {
-            entity.id = String.valueOf(nextId++);
-        }
-        super.save(entity);
-    }
 }
